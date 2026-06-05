b1dabbb33a3b4dfc95c68701ddd02a8cff1c80c4
Ekansh Patil <ekansh.patil23@gmail.com>
2026-06-05 17:02:09 +0100

    Add files via upload

---
+++ SpritesInPygame.py
@@ -0,0 +1,34 @@
+import pygame
+
+WIDTH = 600
+HEIGHT = 600
+TITLE = "SpritesInPygame"
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption(TITLE)
+rocket_img = pygame.image.load("rocket.png")
+space_img = pygame.image.load("space.png")
+run = True
+
+class Rocket(pygame.sprite.Sprite):
+    def __init__(self,image,x,y,):
+        super().__init__()
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+rocket1 = Rocket(rocket_img,300,300)
+
+
+
+sprite_group = pygame.sprite.Group()
+sprite_group.add(rocket1)
+
+while run == True:
+    screen.blit(space_img,(0,0))
+    sprite_group.draw(screen)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    
+
+    pygame.display.update()
